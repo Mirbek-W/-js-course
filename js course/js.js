944323ed3417669d284d5e994cb9e69879721a4b
Mirbek <81816961+Mirbek-W@users.noreply.github.com>
2022-07-19 12:35:51 +0300

    add console log

--- js course/js.js
+++ js course/js.js
@@ -1,5 +1,13 @@
 function iop23(){
 
     const getH2 = document.querySelector('h2')
-    getH2.classList.add('color')
+    getH2.classList.toggle('color')
+
+}
+
+function iop24(){
+
+    const getH2 = document.querySelector('.blok1')
+    getH2.classList.toggle('color')
+    
 }
